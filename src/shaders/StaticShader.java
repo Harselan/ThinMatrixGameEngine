81fbbf4fe8458d92e495a1888aa37401fc2bc724
Harselan <haeressel@gmail.com>
2018-08-25 09:29:11 +0200

    Har implementerat shaders till programmet så att man kan få ut olika färger på modellerna

---
+++ src/shaders/StaticShader.java
@@ -0,0 +1,18 @@
+package shaders;
+
+public class StaticShader extends ShaderProgram
+{
+	public static final String VERTEX_FILE = "src/shaders/vertexShader.vs";
+	public static final String FRAGMENT_FILE = "src/shaders/fragmentShader.fs";
+
+	public StaticShader() 
+	{
+		super( VERTEX_FILE, FRAGMENT_FILE );
+	}
+
+	@Override
+	protected void bindAttributes() 
+	{		
+		super.bindAttribute( 0, "position" );
+	}
+}
