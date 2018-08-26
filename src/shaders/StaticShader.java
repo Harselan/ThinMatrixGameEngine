bfce2191f94c0c8c7adccd532b6e1b5006cc78de
Harselan <haeressel@gmail.com>
2018-08-26 10:50:13 +0200

    Har påbörjat att lägga till uniformer och matrixer i uträkningen av positioner för modellerna

--- src/shaders/StaticShader.java
+++ src/shaders/StaticShader.java
@@ -1,9 +1,13 @@
 package shaders;
 
+import org.lwjgl.util.vector.Matrix4f;
+
 public class StaticShader extends ShaderProgram
 {
 	public static final String VERTEX_FILE = "src/shaders/vertexShader.vs";
 	public static final String FRAGMENT_FILE = "src/shaders/fragmentShader.fs";
+	
+	private int location_transformationMatrix;
 
 	public StaticShader() 
 	{
@@ -15,5 +19,15 @@ public class StaticShader extends ShaderProgram
 	{		
 		super.bindAttribute( 0, "position" );
 		super.bindAttribute( 1, "textureCoords" );
+	}
+
+	@Override
+	protected void getAllUniformLocations() {
+		location_transformationMatrix = super.getUniformLocation( "transformatinoMatrix" );
+	}
+	
+	public void loadTransformationMatrix( Matrix4f matrix )
+	{
+		super.loadMatrix( location_transformationMatrix, matrix );
 	}
 }
