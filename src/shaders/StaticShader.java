10f3bec8492a9319bf9c0b481fc380811381027c
Harselan <haeressel@gmail.com>
2018-08-26 15:45:45 +0200

    Har fixat till en kamera samt gjort klart uniformladdningen

--- src/shaders/StaticShader.java
+++ src/shaders/StaticShader.java
@@ -2,12 +2,17 @@ package shaders;
 
 import org.lwjgl.util.vector.Matrix4f;
 
+import entities.Camera;
+import toolBox.Maths;
+
 public class StaticShader extends ShaderProgram
 {
 	public static final String VERTEX_FILE = "src/shaders/vertexShader.vs";
 	public static final String FRAGMENT_FILE = "src/shaders/fragmentShader.fs";
 	
 	private int location_transformationMatrix;
+	private int location_projectionMatrix;
+	private int location_viewMatrix;
 
 	public StaticShader() 
 	{
@@ -23,11 +28,23 @@ public class StaticShader extends ShaderProgram
 
 	@Override
 	protected void getAllUniformLocations() {
-		location_transformationMatrix = super.getUniformLocation( "transformatinoMatrix" );
+		location_transformationMatrix 	= super.getUniformLocation( "transformationMatrix" );
+		location_projectionMatrix 		= super.getUniformLocation( "projectionMatrix" );
+		location_viewMatrix 			= super.getUniformLocation( "viewMatrix" );
 	}
 	
 	public void loadTransformationMatrix( Matrix4f matrix )
 	{
 		super.loadMatrix( location_transformationMatrix, matrix );
+	}
+	
+	public void loadViewMatrix( Camera camera )
+	{
+		super.loadMatrix( location_viewMatrix, Maths.createViewMatrix( camera ) );
+	}
+	
+	public void loadProjectionMatrix( Matrix4f projection )
+	{
+		super.loadMatrix( location_projectionMatrix, projection );
 	}
 }
