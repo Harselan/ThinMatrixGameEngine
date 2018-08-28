5b66a9dedffeaafbb79648936ec2a7c615e17e34
Harselan <haeressel@gmail.com>
2018-08-28 20:37:24 +0200

    Har lagt till ljus

--- src/engineTester/MainGameLoop.java
+++ src/engineTester/MainGameLoop.java
@@ -5,6 +5,7 @@ import org.lwjgl.util.vector.Vector3f;
 
 import entities.Camera;
 import entities.Entity;
+import entities.Light;
 import models.RawModel;
 import models.TexturedModel;
 import renderEngine.DisplayManager;
@@ -24,13 +25,12 @@ public class MainGameLoop {
 		StaticShader shader = new StaticShader();
 		Renderer renderer = new Renderer( shader );
 		
-		
-		
 		RawModel model = OBJLoader.loadObjModel( "stall", loader );
 
 		TexturedModel staticModel = new TexturedModel( model, new ModelTexture( loader.loadTexture( "stallTexture" ) ) );
 		
 		Entity entity = new Entity( staticModel, new Vector3f( 0, 0, -20 ), 0, 0, 0, 1 );
+		Light light = new Light( new Vector3f( 0, 0, -20 ), new Vector3f( 1,1,1 ) );
 		
 		Camera camera = new Camera();
 		
@@ -40,6 +40,7 @@ public class MainGameLoop {
 			camera.move();
 			renderer.prepare();
 			shader.start();
+			shader.loadLight( light );
 			shader.loadViewMatrix( camera );
 			renderer.render( entity, shader );
 			shader.stop();
--- src/renderEngine/Renderer.java
+++ src/renderEngine/Renderer.java
@@ -45,6 +45,7 @@ public class Renderer
 		GL30.glBindVertexArray( rawModel.getVaoID() );
 		GL20.glEnableVertexAttribArray( 0 );
 		GL20.glEnableVertexAttribArray( 1 );
+		GL20.glEnableVertexAttribArray( 2 );
 		
 		Matrix4f transformationMatrix = Maths.createTransformationMatrix( entity.getPosition(), entity.getRotX(), entity.getRotY(), entity.getRotZ(), entity.getScale() );
 		
@@ -53,6 +54,7 @@ public class Renderer
 		GL13.glActiveTexture( GL13.GL_TEXTURE0 );
 		GL11.glBindTexture( GL11.GL_TEXTURE_2D, model.getTexture().getID() );
 		GL11.glDrawElements( GL11.GL_TRIANGLES, rawModel.getVertexCount(), GL11.GL_UNSIGNED_INT, 0 );
+		GL20.glDisableVertexAttribArray( 2 );
 		GL20.glDisableVertexAttribArray( 1 );
 		GL20.glDisableVertexAttribArray( 0 );
 		GL30.glBindVertexArray( 0 );
